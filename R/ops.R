86e2f7d679e740c5c3cc540f80d56903806b58b8
ajvandervoorn <ajvandervoorn@gmail.com>
2021-06-11 12:33:29 +1200

    update repo

--- R/ops.R
+++ R/ops.R
@@ -215,7 +215,6 @@ op_font_def <- function(op) {
     
     if (is.null(fonts[[fontnum]]) || !(identical_font(op, fonts[[fontnum]]$op))) {
         
-        
         fontname <- paste(blockValue(op$blocks$op.opparams.fontname.name),
                           collapse="")
         engine <- get("engine")
@@ -233,11 +232,11 @@ op_font_def <- function(op) {
 identical_font <- function(op1, op2) {
     
     identical(blockValue(op1$blocks$op.opcode), blockValue(op2$blocks$op.opcode)) &&
-    identical(blockValue(op1$blocks$op.opparams.k), blockValue(op2$blocks$op.opparams.k)) &&
-    identical(blockValue(op1$blocks$op.opparams.c), blockValue(op2$blocks$op.opparams.c)) &&
-    identical(blockValue(op1$blocks$op.opparams.s), blockValue(op2$blocks$op.opparams.s)) &&
-    identical(blockValue(op1$blocks$op.opparams.d), blockValue(op2$blocks$op.opparams.d)) &&
-    identical(blockValue(op1$blocks$op.opparams.fontname.name), blockValue(op2$blocks$op.opparams.fontname.name))
+        identical(blockValue(op1$blocks$op.opparams.k), blockValue(op2$blocks$op.opparams.k)) &&
+        identical(blockValue(op1$blocks$op.opparams.c), blockValue(op2$blocks$op.opparams.c)) &&
+        identical(blockValue(op1$blocks$op.opparams.s), blockValue(op2$blocks$op.opparams.s)) &&
+        identical(blockValue(op1$blocks$op.opparams.d), blockValue(op2$blocks$op.opparams.d)) &&
+        identical(blockValue(op1$blocks$op.opparams.fontname.name), blockValue(op2$blocks$op.opparams.fontname.name))
 
 }
 
